eaa3355344ae3dc36ee0d26eb36df306ea58a31c
fola60 <afolabiadekanle@gmail.com>
2025-09-26 16:50:58 +0100

    email change

--- hello.py
+++ hello.py
@@ -12,6 +12,6 @@ def about():
     return '<p>This application is running on the Flask web framework.</p><p>Learn more about Flask at <a href="https://flask.palletsprojects.com/">https://flask.palletsprojects.com/</a></p><p>Learn more about Python at <a href="https://www.python.org/">https://www.python.org/</a></p><p><a href="/">Back to home</a></p>'
 @app.route("/contact")
 def contact():
-    return '<p> My email is afolabiadekanle@gmail.com <p>'
+    return '<p> My email is c23330061@gmail.com <p>'
 
 
