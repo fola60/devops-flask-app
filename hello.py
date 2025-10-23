ab0b154c9c086f6be7a32ff4c928137a1960a3ce
fola60 <afolabiadekanle@gmail.com>
2025-10-23 19:48:49 +0100

    compose + redis

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import redis
+import time
 
+r = redis.Redis(host='redis-server', port=6379)
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +12,18 @@ def say_hello():
 
 @app.route('/about')
 def about():
-    return '<p>This application is running on the Flask web framework.</p><p>Learn more about Flask at <a href="https://flask.palletsprojects.com/">https://flask.palletsprojects.com/</a></p><p>Learn more about Python at <a href="https://www.python.org/">https://www.python.org/</a></p><p><a href="/">Back to home</a></p>'
+    data = r.get('about')
+    if data is not None:
+        if time.time() - data['time'] > 600:
+            return data['html']
+
+    data = '<p>This application is running on the Flask web framework.</p><p>Learn more about Flask at <a href="https://flask.palletsprojects.com/">https://flask.palletsprojects.com/</a></p><p>Learn more about Python at <a href="https://www.python.org/">https://www.python.org/</a></p><p><a href="/">Back to home</a></p>'
+    r.set('about', {
+        'html': data,
+        'time': time.time()
+    })
+    return data
+
 @app.route("/contact")
 def contact():
     return '<p> My email is c23330061@gmail.com <p>'
